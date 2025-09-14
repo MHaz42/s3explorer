8958ce48296ad578458b2c91693c088cc295a6de
Bouchet, Mateo [FR] <mateo.bouchet@mhaz42.fr>
2025-09-14 17:20:22 +0200

    Added datatable and header

--- src/s3explorer/app.py
+++ src/s3explorer/app.py
@@ -1,6 +1,28 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, Placeholder, Input, Label, ListView
+from textual.events import Resize
+from textual.widgets import (
+    Footer,
+    Input,
+    Label,
+    ListView,
+    ListItem,
+    DataTable,
+)
+from textual.widgets.data_table import ColumnKey
 from textual.containers import Container
+
+from s3explorer.version import VERSION
+from datetime import datetime
+
+
+class ExplorerTable(DataTable):
+    def on_resize(self, event: Resize) -> None:
+        self.columns[ColumnKey("filename")].auto_width = False
+        if event.size.width - 45 > 30:
+            self.columns[ColumnKey("filename")].width = event.size.width - 46
+        else:
+            self.columns[ColumnKey("filename")].width = 30
+        self.refresh()
 
 
 class S3Explorer(App):
@@ -10,16 +32,36 @@ class S3Explorer(App):
 
     TITLE = "S3Explorer"
 
+    ROWS = [
+        ("toto.txt", datetime.now(), "55Ko"),
+        ("titi.txt", datetime(2025, 9, 10, 12, 0, 0), "100Ko"),
+    ]
+
     def compose(self) -> ComposeResult:
-        yield Header(icon="*")
         yield Container(
+            Label(f"[b]S3Explorer[/] [dim]{VERSION}[/]", id="header", classes="box"),
             Container(
-                Label("s3://", id="patheditorlabel"),
-                Input(id="patheditorinput", placeholder="S3 path here"),
+                Label("s3://", id="patheditorlabel", classes="box"),
+                Input(id="patheditorinput", placeholder="S3 path here", classes="box"),
                 id="patheditor",
+                classes="box",
             ),
-            ListView(id="bookmarks", name="Bookmarks"),
-            Placeholder(id="explorer"),
+            ListView(
+                ListItem(Label("Bookmark1")),
+                id="bookmarks",
+                name="Bookmarks",
+                classes="box",
+            ),
+            ExplorerTable(id="explorer"),
             id="app",
         )
         yield Footer()
+
+    def on_mount(self):
+        table = self.query_one(ExplorerTable)
+        table.cursor_type = "row"
+        table.zebra_stripes = True
+        table.add_column("filename", key="filename")
+        table.add_column("last edited", width=30)
+        table.add_column("size", width=10)
+        table.add_rows(self.ROWS)
