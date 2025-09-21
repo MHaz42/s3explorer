ca5f7a213360628061d124a2b334680c8c754d64
Cyril Conde <cyril.conde@gmail.com>
2025-09-21 16:55:29 +0200

    Allow running application without installing it

--- src/s3explorer/__main__.py
+++ src/s3explorer/__main__.py
@@ -1,8 +1,19 @@
 """ The main entrypoint module """
 
+import os
+import sys
+
+# Insert source path in PYTHONPATH before importing s3explorer package
+if not __package__:
+    package_source_path = os.path.dirname(os.path.dirname(__file__))
+    sys.path.insert(0, package_source_path)
+
+import logging
+
 from s3explorer.app import S3Explorer
 
 def main():
+    logging.info(f"Starting S3Explorer !!!")
     app = S3Explorer()
     app.run()
 
