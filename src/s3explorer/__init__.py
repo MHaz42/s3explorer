c5e28154f97a67f41f852aad9c89fa19505025bc
Cyril Conde <cyril.conde@gmail.com>
2025-10-05 17:11:51 +0200

    Add logging to app

--- src/s3explorer/__init__.py
+++ src/s3explorer/__init__.py
@@ -1,1 +1,10 @@
-from . import app
+import os
+import logging
+
+
+root_logger = logging.getLogger()
+file_handler = logging.FileHandler(f"{os.getcwd()}/log.txt")
+file_handler.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+file_handler.setLevel(logging.INFO)
+root_logger.addHandler(file_handler)
+root_logger.setLevel(logging.INFO)
